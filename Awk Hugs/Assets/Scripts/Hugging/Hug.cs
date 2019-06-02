8099d2dc4cb25f6b4b7545a1c5a4de506d10374e
Jed Lankitus <jlankitus@gmail.com>
2019-06-01 19:10:28 -0700

    Added a basic ass hug

---
+++ Awk Hugs/Assets/Scripts/Hugging/Hug.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RootMotion.FinalIK;
+
+public class Hug : MonoBehaviour
+{
+    public FullBodyBipedIK ik;
+
+    public Transform leftHandTarget, rightHandTarget;
+
+    private void LateUpdate()
+    {
+        // Set left hand target to be the effector on the left hand
+        ik.solver.leftHandEffector.position = leftHandTarget.position;
+        ik.solver.leftHandEffector.rotation = leftHandTarget.rotation;
+        ik.solver.leftHandEffector.positionWeight = 1f;
+        ik.solver.leftHandEffector.rotationWeight = 1f;
+
+        // Set right hand target to be the effector on the right hand
+        ik.solver.rightHandEffector.position = rightHandTarget.position;
+        ik.solver.rightHandEffector.rotation = rightHandTarget.rotation;
+        ik.solver.rightHandEffector.positionWeight = 1f;
+        ik.solver.rightHandEffector.rotationWeight = 1f;
+    }
+}
