97d4dc474426850e3025861632098764109c5e90
Bryan Irvine <kirbyderby2000@gmail.com>
2019-06-02 21:53:20 -0700

    Added a music player script and added audio clips / music files.

---
+++ Awk Hugs/Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    static MusicPlayer singletonInstance;
+
+    private void Awake()
+    {
+        if(singletonInstance == null)
+        {
+            DontDestroyOnLoad(this.gameObject);
+            singletonInstance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
