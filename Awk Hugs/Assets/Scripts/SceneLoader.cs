b2ecaf57130c647b9cc6485985ed1cec0133d73c
Bryan Irvine <kirbyderby2000@gmail.com>
2019-06-02 21:44:05 -0700

    Added a Main Menu scene. The scene relies on the SceneLoader script that loads the first game level scene.

---
+++ Awk Hugs/Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneLoader : MonoBehaviour
+{
+
+
+    public void LoadScene(int sceneIndex)
+    {
+
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+    }
+
+
+}
